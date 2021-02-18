27517b0ade55b5431130ca40fafedd891dad00f3
Utkarsh Raj Goswami <axekay2425@gmail.com>
2021-02-18 12:28:13 +0530

    Update spark-loader.scala

--- spark-loader.scala
+++ spark-loader.scala
@@ -2,3 +2,6 @@ var formatOptions : Map[String,String] =Map[String,String]()
 formatOptions = formatOptions + ("header" -> "true") + ("delimiter" -> "|") + ("inferSchema" -> "true")
 
 val df = spark.read.format("csv").options(formatOptions).load("s3://my-bucket/data-gen/delimited/202009121*")
+
+
+
